f72bcba5ee20aef5ebf3286c82c5bbba182ab2d6
urlastway <andriy.semenchenko13@gmail.com>
2022-05-25 00:24:27 +0200

    Mensione 1 completa

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        contacts: [
+            {
+                name: 'Michele',
+                img: 'img/avatar_1.jpg',
+                time: '12:00',     
+            },
+            {
+                name:'Fabio',
+                img: 'img/avatar_2.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Samuele',
+                img: 'img/avatar_3.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Alessandro B.',
+                img: 'img/avatar_4.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Alessandro L.',
+                img: 'img/avatar_5.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Claudia',
+                img: 'img/avatar_6.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Federico',
+                img: 'img/avatar_7.jpg',
+                time: '12:00',     
+            },
+            {
+                name: 'Davide',
+                img: 'img/avatar_8.jpg',
+                time: '12:00',     
+            }
+        ],
+    },
+});
