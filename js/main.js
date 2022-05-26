60a6fc3d758217bbf1b4ef36fa08864e0b90315f
urlastway <andriy.semenchenko13@gmail.com>
2022-05-26 15:19:44 +0200

    aggiunta messaggi dinamici

--- js/main.js
+++ js/main.js
@@ -6,42 +6,75 @@ const app = new Vue({
             {
                 name: 'Michele',
                 img: 'img/avatar_1.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                        'Ciao come stai?', 'Tutto bene.',
+                ],
+                mex__user: ['Bene, tu?',],
+
             },
             {
                 name:'Fabio',
                 img: 'img/avatar_2.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'hai finito di fare la doccia???', 'ok',
+                ],
+                mex__user: ['Si, sto scendendo.',],     
             },
             {
                 name: 'Samuele',
                 img: 'img/avatar_3.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'Hai comprato il pane?', 'Prendi anche le fragole.',
+                ],
+                mex__user: ['Sto andando in cassa!',],     
             },
             {
                 name: 'Alessandro B.',
                 img: 'img/avatar_4.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'Sta sera che facciamo?', 'Va bene, ci vediamo alle 18:45.',
+                ],
+                mex__user: ['Andiamo a cena fuori?',],     
             },
             {
                 name: 'Alessandro L.',
                 img: 'img/avatar_5.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'A che ora è la festa da Mirco?', 'Faccio un po tardi.',
+                ],
+                mex__user: ['Alle 20:00', ],    
             },
             {
                 name: 'Claudia',
                 img: 'img/avatar_6.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'Che cosa ti ha detto Marica?', 'Incolpala!',
+                ],
+                mex__user: ['Che si sente in colpa.', ],    
             },
             {
                 name: 'Federico',
                 img: 'img/avatar_7.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'Non ho studiato per interrogazione.', 'Prenderò in considerazione la tua proposta però preferisco saltare scuola.',
+                ],
+                mex__user: ['Tranquillo, ti sugerisce la paperella.',],     
             },
             {
                 name: 'Davide',
                 img: 'img/avatar_8.jpg',
-                time: '12:00',     
+                time: '12:00',
+                mex__contact: [
+                    'Finalmente è arrivata amica mia, la aspettavo da 3 mesi', 'La Paperella di Boolean',
+                ],
+                mex__user: ['E chi è?',],     
             },
         ],
     },
