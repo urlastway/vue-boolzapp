40b3e64aab5c54489fe12cd096202cdd38634fcc
urlastway <andriy.semenchenko13@gmail.com>
2022-05-26 20:21:17 +0200

    push2

--- js/main.js
+++ js/main.js
@@ -7,75 +7,205 @@ const app = new Vue({
                 name: 'Michele',
                 img: 'img/avatar_1.jpg',
                 time: '12:00',
-                mex__contact: [
-                        'Ciao come stai?', 'Tutto bene.',
+                massages: [
+                    {			
+                        massage: 'Ciao come stai?',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Bene, tu?',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Tutto bene.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
                 ],
-                mex__user: ['Bene, tu?',],
+
 
             },
             {
                 name:'Fabio',
                 img: 'img/avatar_2.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'hai finito di fare la doccia???', 'ok',
-                ],
-                mex__user: ['Si, sto scendendo.',],     
+                massages: [
+                    {			
+                        massage: 'hai finito di fare la doccia???',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Si, sto scendendo.',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'ok',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ],   
             },
             {
                 name: 'Samuele',
                 img: 'img/avatar_3.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'Hai comprato il pane?', 'Prendi anche le fragole.',
-                ],
-                mex__user: ['Sto andando in cassa!',],     
+                massages: [
+                    {			
+                        massage: 'Hai comprato il pane?',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Sto andando in cassa!',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Prendi anche le fragole.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ],    
             },
             {
                 name: 'Alessandro B.',
                 img: 'img/avatar_4.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'Sta sera che facciamo?', 'Va bene, ci vediamo alle 18:45.',
-                ],
-                mex__user: ['Andiamo a cena fuori?',],     
+                massages: [
+                    {			
+                        massage: 'Che cosa facciamo sta sera?',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Andiamo a cena fuori?',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Va bene, ci vediamo alle 18:45.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ], 
             },
             {
                 name: 'Alessandro L.',
                 img: 'img/avatar_5.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'A che ora è la festa da Mirco?', 'Faccio un po tardi.',
-                ],
-                mex__user: ['Alle 20:00', ],    
+                massages: [
+                    {			
+                        massage: 'A che ora è la festa da Mirco?',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Alle 20:00',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Faccio un po tardi.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ],   
             },
             {
                 name: 'Claudia',
                 img: 'img/avatar_6.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'Che cosa ti ha detto Marica?', 'Incolpala!',
+                massages: [
+                    {			
+                        massage: 'Che cosa ti ha detto Marica?',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Che si sente in colpa.',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Incolpala!',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
                 ],
-                mex__user: ['Che si sente in colpa.', ],    
             },
             {
                 name: 'Federico',
                 img: 'img/avatar_7.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'Non ho studiato per interrogazione.', 'Prenderò in considerazione la tua proposta però preferisco saltare scuola.',
-                ],
-                mex__user: ['Tranquillo, ti sugerisce la paperella.',],     
+                massages: [
+                    {			
+                        massage: 'Non ho studiato per interrogazione.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'Tranquillo, ti sugerisce la paperella.',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'Prenderò in considerazione la tua proposta però preferisco saltare scuola.',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ],    
             },
             {
                 name: 'Davide',
                 img: 'img/avatar_8.jpg',
                 time: '12:00',
-                mex__contact: [
-                    'Finalmente è arrivata amica mia, la aspettavo da 3 mesi', 'La Paperella di Boolean',
-                ],
-                mex__user: ['E chi è?',],     
+                massages: [
+                    {			
+                        massage: 'Finalmente è arrivata amica mia, la aspettavo da 3 mesi',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+
+                    {			
+                        massage: 'E chi è?',
+                        time: '12:00', 
+                        status: 'send',
+                    },
+
+                    {			
+                        massage: 'La Paperella di Boolean',
+                        time: '12:00', 
+                        status: 'received',
+                    },
+                
+                ],   
             },
         ],
     },
+    
 });
