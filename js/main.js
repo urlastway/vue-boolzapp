e95d0ea9077a3d980c4523cbdaec530c4ca56cf5
urlastway <andriy.semenchenko13@gmail.com>
2022-05-27 03:07:11 +0200

    correct

--- js/main.js
+++ js/main.js
@@ -3,106 +3,86 @@ const app = new Vue({
     data: {
         currentIndex: 0,
         contacts: [
-            {
-                name: 'Michele',
-                img: 'img/avatar_1.jpg',
-                time: '12:00',
-                massages: [
-                    {			
-                        massage: 'Ciao come stai?',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-
-                    {			
-                        massage: 'Bene, tu?',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'Tutto bene.',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                    
-                
-                ],
-
-
-            },
-            {
-                name:'Fabio',
-                img: 'img/avatar_2.jpg',
-                time: '12:00',
-                massages: [
-                    {			
-                        massage: 'hai finito di fare la doccia???',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-
-                    {			
-                        massage: 'Si, sto scendendo.',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'ok',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                
-                ],   
-            },
-            {
-                name: 'Samuele',
-                img: 'img/avatar_3.jpg',
-                time: '12:00',
-                massages: [
-                    {			
-                        massage: 'Hai comprato il pane?',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-
-                    {			
-                        massage: 'Sto andando in cassa!',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'Prendi anche le fragole.',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                
-                ],    
-            },
+                {
+                    name: 'Michele',
+                    img: 'img/avatar_1.jpg',
+                    visible: true,
+                    massages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            massage: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            massage: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            massage: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name:'Fabio',
+                    img: 'img/avatar_2.jpg',
+                    visible: true,
+                    massages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            massage: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            massage: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            massage: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                ],  
+                },
+                {
+                    name: 'Samuele',
+                    img: 'img/avatar_3.jpg',
+                    massages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            massage: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            massage: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            massage: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],   
+                },
             {
                 name: 'Alessandro B.',
                 img: 'img/avatar_4.jpg',
                 time: '12:00',
                 massages: [
-                    {			
-                        massage: 'Che cosa facciamo sta sera?',
-                        time: '12:00', 
-                        status: 'received',
+                    {
+                        date: '10/01/2020 15:30:55',
+                        massage: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
                     },
-
-                    {			
-                        massage: 'Andiamo a cena fuori?',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'Va bene, ci vediamo alle 18:45.',
-                        time: '12:00', 
-                        status: 'received',
-                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        massage: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    },        
                 
                 ], 
             },
@@ -111,24 +91,16 @@ const app = new Vue({
                 img: 'img/avatar_5.jpg',
                 time: '12:00',
                 massages: [
-                    {			
-                        massage: 'A che ora è la festa da Mirco?',
-                        time: '12:00', 
-                        status: 'received',
+                    {
+                        date: '10/01/2020 15:30:55',
+                        massage: 'Ricordati di chiamare la nonna',
+                        status: 'sent'
                     },
-
-                    {			
-                        massage: 'Alle 20:00',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'Faccio un po tardi.',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                
+                    {
+                        date: '10/01/2020 15:50:00',
+                        massage: 'Va bene, stasera la sento',
+                        status: 'received'
+                    }                
                 ],   
             },
             {
@@ -136,24 +108,21 @@ const app = new Vue({
                 img: 'img/avatar_6.jpg',
                 time: '12:00',
                 massages: [
-                    {			
-                        massage: 'Che cosa ti ha detto Marica?',
-                        time: '12:00', 
-                        status: 'received',
+                    {
+                        date: '10/01/2020 15:30:55',
+                        massage: 'Ciao Claudia, hai novità?',
+                        status: 'sent'
                     },
-
-                    {			
-                        massage: 'Che si sente in colpa.',
-                        time: '12:00', 
-                        status: 'send',
+                    {
+                        date: '10/01/2020 15:50:00',
+                        massage: 'Non ancora',
+                        status: 'received'
                     },
-
-                    {			
-                        massage: 'Incolpala!',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                
+                    {
+                        date: '10/01/2020 15:51:00',
+                        massage: 'Nessuna nuova, buona nuova',
+                        status: 'sent'
+                    }                
                 ],
             },
             {
@@ -161,24 +130,16 @@ const app = new Vue({
                 img: 'img/avatar_7.jpg',
                 time: '12:00',
                 massages: [
-                    {			
-                        massage: 'Non ho studiato per interrogazione.',
-                        time: '12:00', 
-                        status: 'received',
+                    {
+                        date: '10/01/2020 15:30:55',
+                        massage: 'Fai gli auguri a Martina che è il suo compleanno!',
+                        status: 'sent'
                     },
-
-                    {			
-                        massage: 'Tranquillo, ti sugerisce la paperella.',
-                        time: '12:00', 
-                        status: 'send',
-                    },
-
-                    {			
-                        massage: 'Prenderò in considerazione la tua proposta però preferisco saltare scuola.',
-                        time: '12:00', 
-                        status: 'received',
-                    },
-                
+                    {
+                        date: '10/01/2020 15:50:00',
+                        massage: 'Grazie per avermelo ricordato, le scrivo subito!',
+                        status: 'received'
+                    }               
                 ],    
             },
             {
